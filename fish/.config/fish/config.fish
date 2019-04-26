66beb31a3f81f53dc43098baaffbd4feb55fa022
Niklas Wallerstedt <niklaswallerstedt@gmail.com>
2019-04-26 07:44:38 +0200

    Fix homebrew sbin in PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,6 @@ fish_vi_key_bindings
 
 # Export GPG to work correctly
 set -x GPG_TTY (tty)
+
+# Fix Homebrew's sbin was not found in your PATH but you have installed formulae that put executables in /usr/local/sbin
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
