6130920217384a2ec7f5579396512bb9f32fc4f2
Niklas Wallerstedt <niklaswallerstedt@gmail.com>
2019-11-01 12:20:50 +0100

    Some changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,3 +12,15 @@ set -x GPG_TTY (tty)
 
 # Fix Homebrew's sbin was not found in your PATH but you have installed formulae that put executables in /usr/local/sbin
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+#iTerm2 shell integration
+source ~/.iterm2_shell_integration.(basename $SHELL)
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /Users/niklaswallerstedt/.npm/_npx/47954/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
