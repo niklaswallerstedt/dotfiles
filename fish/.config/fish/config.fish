20f5644b994d1d83cc6f376acac1d55bccf999b4
Niklas Wallerstedt <niklaswallerstedt@gmail.com>
2019-11-06 15:40:29 +0100

    Removes local serverless completion

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -25,14 +25,3 @@ end
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/sls.fish ]; and . /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/sls.fish
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/slss.fish ]; and . /Users/niklaswallerstedt/Development/Telia/telia-sense-web-view-back-end/node_modules/tabtab/.completions/slss.fish
